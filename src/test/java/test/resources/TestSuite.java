6bd0d36bfa13528ecb7806f93ee6e2f6f6603c91
Estres <Estres@192.168.40.141>
2017-03-30 17:13:48 -0300

    test

--- src/test/java/test/resources/TestSuite.java
+++ src/test/java/test/resources/TestSuite.java
@@ -5,7 +5,7 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({ AboutFeature.class, AboutFeatureFirefox.class /*, BookFeature.class, TrackFeature.class*/})
+@SuiteClasses({ AboutFeatureFirefox.class /*, BookFeature.class, TrackFeature.class, AboutFeature.class*/})
 public class TestSuite {
 
 }
