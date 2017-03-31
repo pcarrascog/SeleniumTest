fdc08127db48cef9058bb0c4bb29926cec366d26
Estres <Estres@192.168.40.141>
2017-03-30 21:48:55 -0300

    Prueba test maquina jenkins windows

--- src/test/java/test/resources/TestSuite.java
+++ src/test/java/test/resources/TestSuite.java
@@ -5,7 +5,7 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({ AboutFeatureFirefox.class /*, BookFeature.class, TrackFeature.class, AboutFeature.class*/})
+@SuiteClasses({ AboutFeatureWindows.class /*, BookFeature.class, TrackFeature.class, AboutFeature.class*/})
 public class TestSuite {
 
 }
